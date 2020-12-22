60d6a8d3eb5674d63cd255f52a16cd1248534957
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2020-12-21 21:57:23 -0800

    Adding first function for Timer and timer interval

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+var timeEl = document.querySelector(".time");
+
 var startQuiz = document.querySelector("#start-quiz");
+
 var questionOne = document.querySelector("#question1");
 var questionTwo = document.querySelector("#question2");
 var questionThree = document.querySelector("#question3");
@@ -8,7 +11,20 @@ var questionFive = document.querySelector("#question5");
 var secondsLeft = 0;
 
 //When you click the START button, a timer starts and you are presented with a question
+startQuiz.addEventListener("click", function(){
 
+    secondsLeft = 100;
+
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = "Time Remaining: " + secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            recordScore();
+        }
+    }, 1000);
+});
 
 
 //When you answer the question, you are presented with another question
@@ -23,6 +39,9 @@ var secondsLeft = 0;
 OR IF the timer reaches 0
 Then the game is over.*/
 
+function recordScore() {
+
+}
 
 
 /*When the game is over, THEN you can save your initials and score into local storage.
