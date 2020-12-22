7b428e2e379cdaa33edf70aa66d469f0456d633d
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2020-12-21 20:44:06 -0800

    Added first bit of pseudocoding for JS file

---
+++ script.js
@@ -0,0 +1,22 @@
+//When you click the START button, a timer starts and you are presented with a question
+
+
+
+//When you answer the question, you are presented with another question
+
+
+
+//When you answer incorrectly, then time is subrtracted from the clock (something like 10 sec?)
+
+
+
+/* When you answer all questions the timer will stop 
+OR IF the timer reaches 0
+Then the game is over.*/
+
+
+
+/*When the game is over, THEN you can save your initials and score into local storage.
+Should we explore redirecting to a new HTML page? Or should we switch between divs that
+dynamically change depending on the conditions... That way keeping only one HTML to worry about. */
+
