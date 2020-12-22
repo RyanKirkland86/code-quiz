ebb1bc9ac1ed3f4279e9312d84666aa64a03c0f3
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2020-12-21 22:21:58 -0800

    Added code to hide starting page when the Start Quiz button is clicked

--- script.js
+++ script.js
@@ -12,7 +12,16 @@ var secondsLeft = 0;
 
 //When you click the START button, a timer starts and you are presented with a question
 startQuiz.addEventListener("click", function gameBegins(){
+    
     secondsLeft = 100;
+
+    var startPage = document.querySelector("#start-page");
+    
+    if (startPage.style.display === "none") {
+        startPage.style.display = "block";
+    } else {
+        startPage.style.display = "none";
+    };
 
     var timerInterval = setInterval(function() {
         secondsLeft--;
